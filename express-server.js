4d0a9589df29f1ee9a1a9c779b2527563eb6316f
nikolay <kombarov55@gmail.com>
2020-05-20 23:56:41 +0300

    initial commit

---
+++ express-server.js
@@ -0,0 +1,63 @@
+const morgan = require("morgan")
+const _ = require("lodash")
+
+const app = express();
+app.use(fileUpload({
+    createParentPath: true
+}))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(morgan("dev"))
+
+// Настройка сервера Express
+const options = {
+    cert: fs.readFileSync('./sslcert/fullchain.pem'),
+    key: fs.readFileSync('./sslcert/privkey.pem')
+};
+
+app.get("/", function(req, res) {
+    res.send("hello world")
+})
+
+
+///
+
+app.post('/upload', async (req, res) => {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+            let avatar = req.files.avatar;
+
+            //Use the mv() method to place the file in upload directory (i.e. "uploads")
+            avatar.mv('./uploads/' + avatar.name);
+
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: avatar.name,
+                    mimetype: avatar.mimetype,
+                    size: avatar.size
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+///
+
+
+const httpServer = http.createServer(app)
+const httpsServer = https.createServer(options, app)
+
+httpServer.listen(8081)
+httpsServer.listen(8082)
