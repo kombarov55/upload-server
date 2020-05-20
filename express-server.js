b804fb5094d5b491a47fc709ab60327f506632dc
nikolay <kombarov55@gmail.com>
2020-05-21 02:05:14 +0300

    added multer impl

--- express-server.js
+++ express-server.js
@@ -7,15 +7,38 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const morgan = require("morgan")
 const _ = require("lodash")
+const multer = require("multer")
 
 const app = express();
 app.use(fileUpload({
     createParentPath: true
 }))
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan("dev"))
+
+const storage = multer.diskStorage({
+	destination: function(req, file, cb) {
+		cb(null, "public")
+	},
+	filename: function(req, file, cb) {
+		cb(null, Date.now() + "-" + file.originalname)
+	}
+})
+
+const upload = multer({storage: storage}).single("file")
+app.post("/upload", (req, res) => {
+	upload(req, res, err => {
+		if (err instanceof multer.MulterError) {
+			return res.status(500).json(err)
+		} else if (err) {
+			return res.status(500).json(err)
+		}
+		return res.status(200).send(req.file)
+	})
+})
 
 // Настройка сервера Express
 const options = {
@@ -26,42 +49,6 @@ const options = {
 app.get("/", function(req, res) {
     res.send("hello world")
 })
-
-
-///
-
-app.post('/upload', async (req, res) => {
-    try {
-        if(!req.files) {
-            res.send({
-                status: false,
-                message: 'No file uploaded'
-            });
-        } else {
-            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-            let avatar = req.files.avatar;
-
-            //Use the mv() method to place the file in upload directory (i.e. "uploads")
-            avatar.mv('./uploads/' + avatar.name);
-
-            //send response
-            res.send({
-                status: true,
-                message: 'File is uploaded',
-                data: {
-                    name: avatar.name,
-                    mimetype: avatar.mimetype,
-                    size: avatar.size
-                }
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-///
-
 
 const httpServer = http.createServer(app)
 const httpsServer = https.createServer(options, app)
